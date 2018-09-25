d0c3e02fa4140ffcdffcd185c7a0168d3b3ab618
pcherian10 <pcherian10@gmail.com>
2018-09-25 13:25:26 -0400

    First working model. User can log in, choose a car to book and then create booking for the car.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,40 +1,10 @@
 class BookingsController < ApplicationController
   def new
-    @booking = Booking.create(:start_date, :end_date,
+    @booking = Booking.create(:start_date => params[:start_date], :end_date => params[:end_date],
       :user_id => params[:user_id],
-      :attraction_id => params[:attraction_id]
+      :car_id => params[:car_id]
     )
     redirect_to user_path(@booking.user)
   end
 
 end
-
-
-=begin
-
-class PostsController < ApplicationController
-  def show
-    @post = Post.find(params[:id])
-  end
-
-  def index
-    @posts = Post.all
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    post = Post.create(post_params)
-    redirect_to post
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :content, :category_ids => [], :categories_attributes => [:name])
-  end
-
-end
-=end
