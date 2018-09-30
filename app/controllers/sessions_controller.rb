c2e81fcfde28352f163927fe2f302682ab70e742
pcherian10 <pcherian10@gmail.com>
2018-09-30 12:00:14 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
+    user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_path
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_23_211032) do
+ActiveRecord::Schema.define(version: 2018_09_30_155432) do
 
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2018_09_23_211032) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
   end
 
 end
