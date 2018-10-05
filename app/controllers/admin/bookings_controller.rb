6b54a5d3de61de71d312d745c3e0d6866fca915f
pcherian10 <pcherian10@gmail.com>
2018-10-05 14:19:19 -0400

    Added admin controller to view bookings stats and details. Additionally, only admins will be able to add cars.

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -2,7 +2,11 @@ class Admin::BookingsController < ApplicationController
 
   def index
     @bookings = Booking.all
+    @cars = Car.all
   end
 
+  def show
+    @booking = Booking.find_by_id(params[:id])
+  end
 
 end
